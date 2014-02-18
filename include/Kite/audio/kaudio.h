5db5fca26025b47489243cdfc7f807cb0f887311
Milad Rasaneh <milad_rasaneh2000@yahoo.com>
2014-02-18 18:28:23 +0330

    added global header for audio module.

---
+++ include/Kite/audio/kaudio.h
@@ -0,0 +1,26 @@
+/*
+ Kite2D Game Framework.
+ Copyright (C) 2010-2014  Milad Rasaneh <milad_rasaneh2000@yahoo.com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef KAUDIO_H
+#define KAUDIO_H
+
+#include "Kite/audio/kaudiotypes.h"
+#include "Kite/audio/kaudiostructs.h"
+#include "Kite/audio/klistener.h"
+#include "Kite/audio/ksoundbuffer.h"
+
+#endif // KAUDIO_H

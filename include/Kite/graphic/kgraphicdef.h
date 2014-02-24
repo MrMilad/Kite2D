21cd93291f334fba803cf2a917e0c22aef29696a
Milad Rasaneh <milad_rasaneh2000@yahoo.com>
2014-02-24 20:35:53 +0330

    added macroes for converting coordinate systems. (opengl/window)

--- include/Kite/graphic/kgraphicdef.h
+++ include/Kite/graphic/kgraphicdef.h
@@ -20,7 +20,7 @@
 
 namespace Kite{
 
-/* Common math constants */
+// Common math constants
 #define KMATH_PI	3.14159265358979323846f
 #define KMATH_PI_2	1.57079632679489661923f
 #define KMATH_PI_4	0.785398163397448309616f
@@ -31,7 +31,7 @@ namespace Kite{
 #define KMATH_PIsub240 0.01308996939f
 #define KMATH_PIsub360 0.00872664626f
 
-/* Hardware color macros */
+// Hardware color macros
 #define ARGB(a,r,g,b)	((DWORD(a)<<24) + (DWORD(r)<<16) + (DWORD(g)<<8) + DWORD(b))
 #define GETA(col)		((col)>>24)
 #define GETR(col)		(((col)>>16) & 0xFF)
@@ -42,8 +42,16 @@ namespace Kite{
 #define SETG(col,g)		(((col) & 0xFFFF00FF) + (DWORD(g)<<8))
 #define SETB(col,b)		(((col) & 0xFFFFFF00) + DWORD(b))
 
-/* buffer offest */
+// buffer offest
 #define KBUFFER_OFFSET(i) ((void*)(i))
+
+// convert opengl coordinate to window coordinate
+#define KCGL_TO_WIN_X(x, w)         (((float)x + 1) * (w / 2))
+#define KCGL_TO_WIN_Y(y, h)         (h - (((float)y + 1) * (h / 2)))
+
+// convert window coordinate to opengl coordinate
+#define KCWIN_TO_GL_X(x, w)         ((x - ((float)w / 2)) * (1 / ((float)w / 2)))
+#define KCWIN_TO_GL_Y(y, h)         ((y - ((float)h / 2)) * (1 / ((float)h / 2)) * -1)
 
 }
 #endif // KGRAPHICDEF_H

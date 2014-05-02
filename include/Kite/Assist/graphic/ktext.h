06914172ba8a7575ddcc0ddb762da1aadd1f44ee
Milad Rasaneh <milad_rasaneh2000@yahoo.com>
2014-05-02 23:04:56 +0430

    ktext added.

---
+++ include/Kite/Assist/graphic/ktext.h
@@ -0,0 +1,60 @@
+/*
+ Kite2D Game Framework.
+ Copyright (C) 2010-2014  Milad Rasaneh <milad_rasaneh2000@yahoo.com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef KTEXT_H
+#define KTEXT_H
+
+#include "Kite/Core/system/ksystemdef.h"
+#include "Kite/Core/graphic/kgraphicstructs.h"
+#include "Kite/Core/math/kvector2.h"
+#include "Kite/Assist/graphic/kquad.h"
+#include <vector>
+#include <cstring>
+
+namespace Kite{
+    class KITE_FUNC_EXPORT KText{
+	public:
+        KText(const std::vector<KAtlasObject> &Font);
+        ~KText();
+
+        inline void setFont(const std::vector<KAtlasObject> &Font) {_kfont = &Font; _kneedUpdate = true;}
+
+        inline const std::vector<KAtlasObject> &getFont() const {return *_kfont;}
+
+        inline void setText(const std::string &Text) {_ktext = Text; _kneedUpdate = true;}
+
+        inline const std::string &getText() const {return _ktext;}
+
+		void setPositon(const KVector2F32 &Position);
+
+		inline const KVector2F32 &getPosition() const { return _kposition; }
+
+		inline F32 getWidth() const { return _kwidth; }
+
+        const std::vector<KQuad> &getQuads();
+
+    private:
+        const std::vector<KAtlasObject> *_kfont;
+        std::vector<KQuad> _kquads;
+        std::string _ktext;
+        KVector2F32 _kposition;
+		F32 _kwidth;
+        bool _kneedUpdate;
+    };
+}
+
+#endif // KTEXT_H
---
+++ src/Kite/Assist/graphic/ktext.cpp
@@ -0,0 +1,77 @@
+/*
+ Kite2D Game Framework.
+ Copyright (C) 2010-2014  Milad Rasaneh <milad_rasaneh2000@yahoo.com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "Kite/Assist/graphic/ktext.h"
+
+namespace Kite{
+	KText::KText(const std::vector<KAtlasObject> &Font) :
+		_kfont(&Font),
+		_ktext(""),
+		_kposition(),
+		_kwidth(0.0f),
+        _kneedUpdate(true)
+    {}
+
+    KText::~KText(){}
+
+	void KText::setPositon(const KVector2F32 &Position){
+		KQuad *qtemp;
+		_kposition = Position;
+		_kwidth = 0.0f;
+		for (U32 i = 0; i < _kquads.size(); i++){
+			qtemp = &_kquads[i];
+
+			qtemp->setPosition(KVector2F32(_kposition.x + _kwidth, _kposition.y));
+			_kwidth += qtemp->getQuadAttribute()->topRight.x;
+		}
+	}
+
+    const std::vector<KQuad> &KText::getQuads(){
+        if (_kneedUpdate){
+            const KAtlasObject *atemp;
+            KQuad *qtemp;
+            KQuadAttrib attrib;
+			char ascii;
+			
+			_kquads.resize(_ktext.size());
+            attrib.bottomLeft = KVector2F32(0.0f, 0.0f);
+
+			for (U32 i = 0; i < _kquads.size(); i++){
+				ascii = _ktext[i];
+				atemp = &_kfont->at(ascii - 32);
+                qtemp = &_kquads[i];
+
+                //size
+				attrib.topRight.x = atemp->w;
+				attrib.topRight.y = atemp->h;
+
+                // uv
+				attrib.bottomLeftUV.x = atemp->blu;
+				attrib.bottomLeftUV.y = atemp->blv;
+				attrib.topRightUV.x = atemp->tru;
+				attrib.topRightUV.y = atemp->trv;
+
+                qtemp->setQuadAttribute(attrib);
+
+            }
+			// set position
+			setPositon(_kposition);
+            _kneedUpdate = false;
+        }
+        return _kquads;
+    }
+}
